dca2f8799f4699128b0a8cadfcdf1e7b3797ab02
Kavin-Antony <kavinantony678>
2026-01-28 12:14:28 +0530

    added real-time counter

--- lib/live_counter_web/live/counter_live.ex
+++ lib/live_counter_web/live/counter_live.ex
@@ -2,8 +2,13 @@ defmodule LiveCounterWeb.CounterLive do
   use LiveCounterWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
+
     {:ok, assign(socket, count: 0)}
   end
+
 
   def render(assigns) do
     ~H"""
@@ -21,6 +26,12 @@ defmodule LiveCounterWeb.CounterLive do
   def handle_event("dec", _params, socket) do
     {:noreply, update(socket, :count, &(&1 - 1))}
   end
+
+  def handle_info(:tick, socket) do
+    {:noreply, update(socket, :count, &(&1 + 1))}
+  end
+
+
 
 
 end
